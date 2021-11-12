21a2665a1fce342a4e8336f00ddcb3a144c2ba16
vlasisPi <vlassisp89@gmail.com>
2021-11-12 13:54:52 +0200

    Minor fix to run db:migrate

--- db/migrate/20211111151751_add_style_to_products.rb
+++ db/migrate/20211111151751_add_style_to_products.rb
@@ -1,5 +1,5 @@
 class AddStyleToProducts < ActiveRecord::Migration[6.1]
   def change
-    add_reference :products, :style, null: false, foreign_key: true
+    add_reference :products, :style
   end
 end
